cd5d1690eec95c56a836c75d90bd6f9cb25e6cba
dayaung2022-del <dayaung.2022@gmail.com>
2026-09-10 22:45:11 +0630

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,14 +2,25 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
+
 android {
     namespace = "com.anynet.admin"
     compileSdk = 35
+
     defaultConfig {
         applicationId = "com.anynet.admin"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
